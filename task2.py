9a8927f17b19eecd3e05c163c9124331ce4f2a58
SofiaEseva <98576496+SofiaEseva@users.noreply.github.com>
2022-02-01 12:35:33 +0500

    Revert "Add files via upload"

--- task2.py
+++
@@ -1,5 +0,0 @@
-import time
-a = int(input('Введите время в секундах: '))
-time_format = time.strftime('%H:%M:%S', time.gmtime(a))
-print('Количество введеных секунд составляет ',time_format)
-
--- task3.py
+++
@@ -1,5 +0,0 @@
-n = int(input('Введите целое число n '))
-temp = str(n)
-nn = int(temp + temp)
-nnn = int(temp + temp + temp)
-print('Сумма чисел в формате n+nn+nnn = ', n + nn + nnn)
